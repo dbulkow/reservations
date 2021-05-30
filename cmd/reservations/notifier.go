bee0e40fb2abb24597e13506d0d99e5be45e41f3
David Bulkow <dbulkow@gmail.com>
2021-05-29 20:57:26 -0400

    add some notes for a notifier mechanism

---
+++ cmd/reservations/notifier.go
@@ -0,0 +1,13 @@
+package main
+
+// send email once a week to each user with active loans and Reservations
+//            on the morning when a reservation is to expire on that day
+//            an hour before a reservation expires
+//            on the morning a reservation is to become active on that day
+//            an hour before a reservation goes active
+
+type notifier struct{}
+
+func (n *notifier) weekly()            {}
+func (n *notifier) daily()             {}
+func (n *notifier) send(target string) {}
